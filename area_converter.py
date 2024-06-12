1ab2f2b46527874302340610141bd96743a6cada
laistdomiciano <laistdomiciano@gmail.com>
2024-06-12 11:59:49 +0200

    saving

--- area_converter.py
+++ area_converter.py
@@ -1,5 +1,1 @@
 def square_meters_to_square_kilometers(square_meters):
-    return square_meters / 1_000_000
-
-square_meters = 1000000
-print(f"{square_meters} square meters is {square_meters_to_square_kilometers(square_meters)} square kilometers")
