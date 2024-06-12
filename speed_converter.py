5fb18d2737c5bc1e99abd84f78ed4459cb129943
laistdomiciano <laistdomiciano@gmail.com>
2024-06-12 12:22:19 +0200

    define mps_to_kph function in speed_converter.py

--- speed_converter.py
+++ speed_converter.py
@@ -1,1 +1,7 @@
-def pass
+def mps_to_kph():
+    return meters_per_second / kph
+
+
+ # Speed conversions
+    meters_per_second = 10
+    print(f"{meters_per_second} meters per second is {mps_to_kph(meters_per_second)} kilometers per hour")
