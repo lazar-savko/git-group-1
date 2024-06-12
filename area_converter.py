9c633109499f82d7c4591f52fcd868683a9754d8
laistdomiciano <laistdomiciano@gmail.com>
2024-06-12 11:58:52 +0200

    One commit

--- area_converter.py
+++ area_converter.py
@@ -1,2 +1,5 @@
-def area_converter():
-    pass
+def square_meters_to_square_kilometers(square_meters):
+    return square_meters / 1_000_000
+
+square_meters = 1000000
+print(f"{square_meters} square meters is {square_meters_to_square_kilometers(square_meters)} square kilometers")
